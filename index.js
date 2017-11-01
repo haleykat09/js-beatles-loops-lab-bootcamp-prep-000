9f599d16f53dbbcc8346c0fbed9c0ac28b5e8e2d
Haley West <haleykat09@yahoo.com>
2017-11-01 23:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var newArray = []
   do {
-    newArray.push("I love the Beatles!")
+    newArray.push("I love the Beatles!") && number++
   }
-  while (number < 15)
+  while (number < 15);
 
   return (newArray)
 }
